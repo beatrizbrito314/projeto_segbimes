ad4023096025c0f35251a897e18a55744e48cab9
beatrizbrito314 <emillybea314@gmail.com>
2023-07-26 15:28:25 -0300

    9min. 3 video

--- carrinho.php
+++ carrinho.php
@@ -29,13 +29,11 @@ if(isset($_POST["comprar"])){
         }  
     }
 if($ni>0){
-    for($i=0;$i<$ni;$i++){
-       echo $i. " X " . $_SESSION['itens'][$i]['desc'] . "-". $_SESSION['itens'][$i]['preco']. "<br>";
-
-    }
+    $_SESSION['valortotal']= $valortotal;
+    header('location: usuario.php',true,303);
 }
-echo $valortotal;
-$_SESSION['valortotal']= $valortotal;
+// echo 'valor total'. $valortotal;
+
 }
 ?>
 <!DOCTYPE html>
@@ -64,21 +62,21 @@ $_SESSION['valortotal']= $valortotal;
             <td>1</td>
             <td><input type="checkbox" name="ch0" ></td>
             <td><input type="text" name="desc0" value="produto1" readonly></td>
-            <td><input type="numb" name="qtd0" value="0" ></td>
+            <td><input type="number" name="qtd0" value="0" ></td>
             <td><input type="text" name="preco0" value="99" readonly ></td>
         </tr>
         <tr>
             <td>2</td>
             <td><input type="checkbox" name="ch1" ></td>
             <td><input type="text" name="desc1" value="produto2" readonly></td>
-            <td><input type="numb" name="qtd1" value="0" ></td>
+            <td><input type="number" name="qtd1" value="0" ></td>
             <td><input type="text" name="preco1" value="99" readonly ></td>
         </tr>
         <tr>
             <td>3</td>
             <td><input type="checkbox" name="ch2" ></td>
             <td><input type="text" name="desc2" value="produto3" readonly></td>
-            <td><input type="numb" name="qtd2" value="0" ></td>
+            <td><input type="number" name="qtd2" value="0" ></td>
             <td><input type="text" name="preco2" value="99" readonly ></td>
         </tr>
         <tr>
---
+++ usuario.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+    $i=1;
+    foreach($_SESSION ['itens'] as $item){
+        echo $i. " X " . $item['desc'] . "-". $item['preco']. "<br>";
+    $i++;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cssteste.css">
+    <title>Document</title>
+</head>
+<body>
+    <form action="usuario.php" method="post">
+        <table>
+            <tr>
+                <td colspan="2">Dados do Usuário</td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+        </table>
+    </form>
+
+
+
+
+
+</body>
+</html>
